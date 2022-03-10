46f7e1fe4e1152eed65ed3b5deb564e7333a7a9e
Jocyline Magero <jmagero54@gmail.com>
2022-03-10 13:14:29 -0600

    Add correct routes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Switch, Route} from 'react-router-dom';
 import { PageOne, PageTwo, PageThree, PageFour } from './pages';
 
 function App() {
   return (
-      <Routes>
-        <Route exact path="/" element ={ <PageOne/>} >
-          <Route path="/pagetwo" element={<PageTwo/>} />
-          <Route path="/pagethree" element={ <PageThree/> } />
-          <Route path="/pagefour" element={ <PageFour/>} />
-          </Route>
-      </Routes>
+         <Switch>
+            <Route exact path="/" component={ PageOne } />
+            <Route path="/pagetwo" component={ PageTwo }/>
+            <Route path="/pagethree" component={ PageThree } />
+            <Route path="/pagefour" component={PageFour} />
+        </Switch>
   );
 }
 
