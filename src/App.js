6a73bb338afb19b34579c2472fb9dc35c13539c4
Jocyline Magero <jmagero54@gmail.com>
2022-03-09 19:50:21 -0600

    Add routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { PageOne, PageTwo, PageThree, PageFour } from './pages';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Routes>
+        <Route exact path="/" element ={ <PageOne/>} >
+          <Route path="/pagetwo" element={<PageTwo/>} />
+          <Route path="/pagethree" element={ <PageThree/> } />
+          <Route path="/pagefour" element={ <PageFour/>} />
+          </Route>
+      </Routes>
   );
 }
 
